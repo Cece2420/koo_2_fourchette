e45e9af36bae1f2d8ff34158c996c8eab7e2d321
Cece2420 <celine@zhang.fr>
2026-05-06 10:24:12 +0200

    2

--- config.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-$pdo = new PDO("mysql:host=127.0.0.1;port=3306;dbname=koo_2_fourchette;charset=utf8mb4","site","site1234");
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
